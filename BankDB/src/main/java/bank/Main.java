c3b5d242125615bf96f189cc0c8d6e36bca99be7
Etkel <118313778+Etkel@users.noreply.github.com>
2023-02-09 01:23:30 +0200

    Update Main.java

--- BankDB/src/main/java/bank/Main.java
+++ BankDB/src/main/java/bank/Main.java
@@ -1,6 +1,11 @@
 package bank;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -32,15 +37,18 @@ public class Main {
                 transferMoney(Currency.USD, bill2, bill3, 74d);
                 //check
                 TypedQuery<Bills> query2 = em.createQuery("SELECT o FROM Bills o WHERE o.id > 1", Bills.class);
+                System.out.println();
                 System.out.println(query2.getResultList());
                 em.getTransaction().commit();
             //Conversion for Bills id = 1;
                 em.getTransaction().begin();
-                ExchangeRates exchangeRates = new ExchangeRates(25.54d, 28.7d);
+                ExchangeRates exchangeRates = new ExchangeRates(25.54d, 28.7d,
+                        25.76d, 28.9d);
                 em.persist(exchangeRates);
-                conversionCurrency(bill1, exchangeRates, Currency.EUR);
+                conversionCurrencyToUAH(bill1, exchangeRates, Currency.EUR);
                 //check
                 TypedQuery<Bills> query3 = em.createQuery("SELECT o FROM Bills o WHERE o.id = 1", Bills.class);
+                System.out.println();
                 System.out.println(query3.getResultList());
                 em.getTransaction().commit();
             //Total balance in UAH
@@ -49,7 +57,20 @@ public class Main {
                 em.persist(client1);
                 client1.setCounts(bill3);
                 bill3.setUser(client1);
+                System.out.println();
                 System.out.println(totalBalanceUAH(client1, exchangeRates));
+                em.getTransaction().commit();
+            //Get real ExchangeRate and get Bills id = 1 buy some EUR
+                em.getTransaction().begin();
+                ExchangeRates privatBank = privatBankExchangeRate();
+                em.persist(privatBank);
+                System.out.println();
+                System.out.println("PrivatBank course is " + privatBank);
+                System.out.println(bill1);
+                conversionCurrencyFromUAH(bill1, privatBank, Currency.EUR);
+                //check
+                TypedQuery<Bills> query4 = em.createQuery("SELECT o FROM Bills o WHERE o.id = 1", Bills.class);
+                System.out.println(query4.getResultList());
                 em.getTransaction().commit();
             } finally {
                 sc.close();
@@ -133,11 +154,11 @@ public class Main {
         }
     }
 
-    public static void conversionCurrency(Bills bills, ExchangeRates exchangeRates, Currency currency) {
+    public static void conversionCurrencyToUAH(Bills bills, ExchangeRates exchangeRates, Currency currency) {
         if (Currency.USD == currency) {
             Query query = em.createQuery("UPDATE Bills b SET b.balanceUAH = " +
                     "b.balanceUSD * :s, b.balanceUSD = 0  WHERE b.countNumber = :id");
-            query.setParameter("s", exchangeRates.getRateUSD());
+            query.setParameter("s", exchangeRates.getBuyUSD());
             query.setParameter("id", bills.getCountNumber());
             query.executeUpdate();
             em.refresh(bills);
@@ -145,7 +166,7 @@ public class Main {
         if (Currency.EUR == currency) {
             Query query = em.createQuery("UPDATE Bills b SET b.balanceUAH = " +
                     "b.balanceEUR * :s, b.balanceEUR = 0 WHERE b.countNumber = :id");
-            query.setParameter("s", exchangeRates.getRateEUR());
+            query.setParameter("s", exchangeRates.getBuyEUR());
             query.setParameter("id", bills.getCountNumber());
             query.executeUpdate();
             em.refresh(bills);
@@ -159,9 +180,50 @@ public class Main {
     public static Double totalBalanceUAH(Client client, ExchangeRates exchangeRates) {
         TypedQuery<Double> count = em.createQuery("SELECT o.balanceUAH + (o.balanceEUR * :e) + " +
                 "(o.balanceUSD * :u) FROM Bills o WHERE o.client.clientKey = :id", Double.class);
-        count.setParameter("e", exchangeRates.getRateEUR());
-        count.setParameter("u", exchangeRates.getRateUSD());
+        count.setParameter("e", exchangeRates.getBuyEUR());
+        count.setParameter("u", exchangeRates.getBuyUSD());
         count.setParameter("id", client.getClientKey());
         return count.getSingleResult();
+    }
+
+    public static ExchangeRates privatBankExchangeRate() {
+        final String url = "https://privatbank.ua/rates-archive";
+        List<Double> buy = new ArrayList<>();
+        List<Double> sale = new ArrayList<>();
+        try {
+            Element info = Jsoup.connect(url).get();
+            for (Element el : info.select("div.purchase span")) {
+                buy.add(Double.parseDouble(el.text()));
+            }
+            for (Element el : info.select("div.sale span")) {
+                sale.add(Double.parseDouble(el.text()));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new ExchangeRates(buy.get(1), buy.get(0), sale.get(1), sale.get(0));
+    }
+
+    public static void conversionCurrencyFromUAH(Bills bills, ExchangeRates exchangeRates, Currency currency) {
+        if (Currency.USD == currency) {
+            Query query = em.createQuery("UPDATE Bills b SET b.balanceUSD = " +
+                    "b.balanceUAH / :s, b.balanceUAH = 0  WHERE b.countNumber = :id");
+            query.setParameter("s", exchangeRates.getSaleUSD());
+            query.setParameter("id", bills.getCountNumber());
+            query.executeUpdate();
+            em.refresh(bills);
+        }
+        if (Currency.EUR == currency) {
+            Query query = em.createQuery("UPDATE Bills b SET b.balanceEUR = " +
+                    "b.balanceUAH / :s, b.balanceUAH = 0 WHERE b.countNumber = :id");
+            query.setParameter("s", exchangeRates.getSaleEUR());
+            query.setParameter("id", bills.getCountNumber());
+            query.executeUpdate();
+            em.refresh(bills);
+        }
+        if (Currency.UAH == currency) {
+            System.out.println("Nothing happen! lol");
+            em.refresh(bills);
+        }
     }
 }
